c72ddcca0d423e0adefcb14a3f0d70bf0c931120
ngochieu276 <ngochieu2761998@gmail.com>
2024-02-17 12:30:32 +0700

    setup route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
-import Layout from "./layout";
-import Collection from "./page/collection";
-import Mainpage from "./page/main-page";
+import Layout from "../layout";
+import Collection from "../page/collection";
+import Mainpage from "../page/main-page";
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 
 export const router = createBrowserRouter(
